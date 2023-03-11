899a907bb7b7973ca5e49552fbeff8fe280bc735
enumer-8 <enumer8@mailfence.com>
2023-03-11 21:51:59 +0800

    added near and far planes to struct and perspective projection function

--- SRC/camera.h
+++ SRC/camera.h
@@ -6,6 +6,9 @@
 #define RAYMATH_IMPLEMENTATION
 #include "raymath.h"
 
+#define N_PLANE 0.1f;
+#define F_PLANE 1000.0f;
+
 typedef struct Camera {
 
 	Vector3 pos;
@@ -22,7 +25,7 @@ Camera* camera_create(Vector3 pos, Vector3 target, Vector3 up, float fov, int SC
 
 void camera_destroy(Camera* camera);
 
-void camera_update_proj(Camera* camera, float fov, int SCR_W, int SCR_H);
+void camera_update_proj(Camera* camera, float fov, int SCR_W, int SCR_H, N_PLANE, F_PLANE);
 
 void camera_pan(Camera* camera, Vector2 delta);
 
