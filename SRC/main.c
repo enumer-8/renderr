3079818714c786823804917d44b88d62087bf160
enumer-8 <enumer8@mailfence.com>
2023-03-17 16:09:24 +0800

    testing line drawing function

--- SRC/main.c
+++ SRC/main.c
@@ -16,30 +16,34 @@
 // Standard headers + Xlib
 #include <stdio.h>      // file writing and general I/O
 #include <stdlib.h>     // memory allocation  
+#include <math.h>       // math functions
 #include <errno.h>      // errors 
 #include <string.h>     // string manipulation
-#include <X11/Xlib.h>   // drawing a window and other UI
 #include <stdbool.h>    // true/false checking
+#include <X11/Xlib.h>   // drawing a window and other UI
 
 // Raymath and custom headers
 #include "raymath.h"    // math functions
-#include "camera.h"     // custom camera implementation
+// #include "camera.h"  // custom camera implementation
 #include "obj.h"        // defined 3D primitives
-#include "bmp.h"        // bitmap structures
-
+// #include "bmp.h"     // bitmap functions
+#include "draw.h"       // drawing functions
 // initialize dimensions 
 #define SCR_W 800
 #define SCR_H 600
 
 // define what file path is to save resulting image - IN PROGRESS
-const char *path_to_file = "output.bmp";
-int width  = SCR_W;
-int height = SCR_H;
+/* const char *path_to_file = "output.bmp"; */
+/* int width  = SCR_W; */
+/* int height = SCR_H; */
 
 // file write function goes here
 
-int save_bitmap_output();
+/* int save_bitmap_output(); */
 
+
+// Line drawing function TEST
+void draw_line(Display* dpy, Window win, GC gc, Line* line);
 		       
 // ====== MAIN LOOP =========================== //
 int main()
@@ -69,6 +73,7 @@ int main()
 
     XEvent ev;  // Declares an XEvent variable to store incoming events
 
+    GC gc;
     
     while (1) // Enters an infinite loop to wait for events
     {  
@@ -78,9 +83,18 @@ int main()
         // Checks if the event is an Expose event (the window needs to be redrawn)
         if (ev.type == Expose) 
         {  
-          
-          // real-time rendering of objects goes HERE 	  
 
+          gc = XCreateGC(dpy, win, 0, NULL);
+	  
+          // Line drawing test
+          Line line;
+          line->p1 = (Vector2) {10,10};
+          line->p2 = (Vector2) {100, 100};
+          line->color = 0xFF0000;
+
+          draw_line(dpy, win, gc, &line);
+     	  
+            
         } 
         else if (ev.type == KeyPress) // Checks if the event is a KeyPress event
         {  
